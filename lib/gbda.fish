fda9fa14005765d3b3ab4aba2ecfe4ca33022523
Patrick Fricano <patrick.fricano@icloud.com>
2023-02-08 12:45:48 -0800

    escape + and * in gbda regex

--- lib/gbda.fish
+++ lib/gbda.fish
@@ -1,5 +1,5 @@
 function gbda -d "Delete all branches merged in current HEAD"
   git branch --merged | \
-    command grep -vE  (echo '^(+|*|\s*('(git_main_branch)'|development|develop|devel|dev)\s*$)') | \
+    command grep -vE  (echo '^(\+|\*|\s*('(git_main_branch)'|development|develop|devel|dev)\s*$)') | \
     command xargs -n 1 git branch -d
 end
